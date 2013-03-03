24a3526424065afb36ace4b8759b0c83850be8b7
Brennan Stehling <brennan@smallsharptools.com>
2013-03-03 04:37:18 -0600

    tweaked Felix icon

--- ADNActivities/ADNFelixActivity.m
+++ ADNActivities/ADNFelixActivity.m
@@ -36,21 +36,20 @@
         //// Color Declarations
         UIColor* fillColor = [UIColor colorWithRed: 1 green: 1 blue: 1 alpha: 1];
         
-        //// Bezier Drawing
-        UIBezierPath* bezierPath = [UIBezierPath bezierPath];
-        [bezierPath moveToPoint: CGPointMake(38.5, 13.5)];
-        [bezierPath addCurveToPoint: CGPointMake(34, 13) controlPoint1: CGPointMake(38.5, 13.5) controlPoint2: CGPointMake(40.01, 13.17)];
-        [bezierPath addCurveToPoint: CGPointMake(22.5, 22.5) controlPoint1: CGPointMake(27.99, 12.83) controlPoint2: CGPointMake(22.5, 22.5)];
-        [bezierPath addCurveToPoint: CGPointMake(12.5, 30.5) controlPoint1: CGPointMake(22.5, 22.5) controlPoint2: CGPointMake(18.6, 30.25)];
-        [bezierPath addCurveToPoint: CGPointMake(4, 21) controlPoint1: CGPointMake(6.4, 30.75) controlPoint2: CGPointMake(4.17, 26.8)];
-        [bezierPath addCurveToPoint: CGPointMake(12, 13) controlPoint1: CGPointMake(3.83, 15.2) controlPoint2: CGPointMake(6.85, 12.91)];
-        [bezierPath addCurveToPoint: CGPointMake(22.5, 19.5) controlPoint1: CGPointMake(17.15, 13.09) controlPoint2: CGPointMake(22.5, 19.5)];
-        [bezierPath addCurveToPoint: CGPointMake(34, 31) controlPoint1: CGPointMake(22.5, 19.5) controlPoint2: CGPointMake(28.99, 31.02)];
-        [bezierPath addCurveToPoint: CGPointMake(38.5, 30.5) controlPoint1: CGPointMake(39.01, 30.98) controlPoint2: CGPointMake(38.5, 30.5)];
-        [bezierPath addLineToPoint: CGPointMake(38.5, 30.5)];
+        //// Alpha Drawing
+        UIBezierPath* alphaPath = [UIBezierPath bezierPath];
+        [alphaPath moveToPoint: CGPointMake(39.5, 14)];
+        [alphaPath addCurveToPoint: CGPointMake(34, 13) controlPoint1: CGPointMake(39.5, 14) controlPoint2: CGPointMake(37.8, 13.04)];
+        [alphaPath addCurveToPoint: CGPointMake(22.5, 22.5) controlPoint1: CGPointMake(30.2, 12.96) controlPoint2: CGPointMake(22.5, 22.5)];
+        [alphaPath addCurveToPoint: CGPointMake(12.5, 30.5) controlPoint1: CGPointMake(22.5, 22.5) controlPoint2: CGPointMake(18.6, 30.25)];
+        [alphaPath addCurveToPoint: CGPointMake(4, 21) controlPoint1: CGPointMake(6.4, 30.75) controlPoint2: CGPointMake(4, 26.5)];
+        [alphaPath addCurveToPoint: CGPointMake(12, 13) controlPoint1: CGPointMake(4, 15.5) controlPoint2: CGPointMake(6.85, 12.91)];
+        [alphaPath addCurveToPoint: CGPointMake(22.5, 19.5) controlPoint1: CGPointMake(17.15, 13.09) controlPoint2: CGPointMake(22.5, 19.5)];
+        [alphaPath addCurveToPoint: CGPointMake(34, 31) controlPoint1: CGPointMake(22.5, 19.5) controlPoint2: CGPointMake(30.09, 31)];
+        [alphaPath addCurveToPoint: CGPointMake(39.5, 30.5) controlPoint1: CGPointMake(37.91, 31) controlPoint2: CGPointMake(39.5, 30.5)];
         [fillColor setStroke];
-        bezierPath.lineWidth = 2.5;
-        [bezierPath stroke];
+        alphaPath.lineWidth = 2.5;
+        [alphaPath stroke];
     }];
     
     return image;
